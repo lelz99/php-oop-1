7cd55c408de7fca6e8f4882ddb093b58e41d7bd4
lelz99 <emanuelesimonetti99@gmail.com>
2024-02-25 13:20:06 +0100

    esercizio svolto

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
+class Movies
+{
+    public $name;
+    public $genre;
+    public $time;
+    public $boxoffice;
+
+    public function __construct($name, $genre, $time, $boxoffice)
+    {
+        $this->name = $name;
+        $this->genre = $genre;
+        $this->time = $time;
+        $this->boxoffice = $boxoffice;
+    }
+
+    public function getDescriptionMovie(Movies $movie)
+    {
+        return "$movie->name il suo genere è $movie->genre e dura $movie->time.";
+    }
+}
+
+$movie1 = new Movies('Ritorno al Futuro', 'Fantascienza', '1h 56m', '367M');
+$movie2 = new Movies('Casinò', 'Noir', '2h 58m', '631M');
+
+echo $movie1->getDescriptionMovie($movie1);
+echo $movie2->getDescriptionMovie($movie2);
+
+
 
 ?>
 
